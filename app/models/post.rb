8a0e959d699fde66bebbc19aabf9e51f9be37fa4
Steven McVey <smcvey1210@gmail.com>
2023-02-21 12:03:18 -0800

    commit tree 4f26f129253e75dec8cf81a6baad4668fb3077b4

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+  validate :title_is_clickbait
+
+  def title_is_clickbait
+    if title
+      unless title.match?(/Won't Believe|Secret|Top+[0-9]|Guess/)
+        errors.add(:title, "Title must be clickbait")
+      end
+    end
+  end
+
 end
